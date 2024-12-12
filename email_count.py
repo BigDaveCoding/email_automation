182ea79681d268bfe42c7c766c73e8defb084580
BigDaveCoding <bigdavecoding@gmail.com>
2024-12-12 12:50:35 +0000

    Update email_count.py

--- email_count.py
+++ email_count.py
@@ -1,11 +1,23 @@
 import imaplib
 import json
 import email
-import email.header from decode_header
+from email.header import decode_header
+
+# Load credentials from JSON file
 
 with open('email_details.json', 'r') as file:
     credentials = json.load('file')
 
 username = credentials['username']
 password = credentials['password']
+
+# Connect to 123-reg's IMAP server
+
+def conncetEmail():
+    mail = imaplib.IMAP4_SSL("imap.123-reg.co.uk")
+    mail.login(username, password)
+    mail.select("inbox")
+
+    print("Connected to email server")
+    return mail
 
